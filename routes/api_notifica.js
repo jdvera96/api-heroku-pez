8d9d9a155ddcfdb9c3a5d1d6c79ce3a333b44a99
jdvera96 <jdvera@espol.edu.ec>
2020-02-18 15:21:30 -0500

    agregacion de mensajes individuales

--- routes/api_notifica.js
+++ routes/api_notifica.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var send=require('../servicios/sendNotifications');
+var sendIndividual=require('../servicios/sendNotificacionsOne');
 
 
 //importo el driver mssql para realizar consultas a la base de datoss
@@ -37,6 +38,22 @@ router.post('/',function(req,res,next){
             res.send([{'respuesta':err}]);
         }else{
             send.servicioNotificacionFCM(campos);
+            res.send(data);
+        }
+    });
+
+});
+
+
+router.post('/individual',function(req,res,next){
+
+    let campos={'token': req.body.token,'titulo': req.body.titulo,'mensaje':req.body.mensaje,'usuario':req.body.usuario,'tipo':req.body.tipo}
+
+    modeloNotificacion.postNotificaciones(campos,function(err,data){
+        if(err){
+            res.send([{'respuesta':err}]);
+        }else{
+            sendIndividual.servicioNotificacionFCM(campos);
             res.send(data);
         }
     });
---
+++ servicios/sendNotificacionsOne.js
@@ -0,0 +1,53 @@
+var modelo = require('../models_sql/modelo_notificacion');
+
+console.log("Prueba individual")
+
+var FCM = require('fcm-node');
+
+
+exports.servicioNotificacionFCM= function(campos){
+
+    var serverKey = 'AIzaSyCDJ18xBiGj0zmEx0c3w3Y-uLc_89LQ85g'; //put your server key here
+    var fcm = new FCM(serverKey);
+    
+    let token = campos.token;
+    //obtengo la fecha y hora actual
+    var hoy = new Date();
+    let ano=hoy.getFullYear();
+    let mes=hoy.getMonth()+1;
+    let dia=hoy.getDay()
+
+    let hora=hoy.getHours();
+    let minutos=hoy.getMinutes();
+
+    let fecha=ano+'-'+mes+'-'+dia+' || '+hora+':'+minutos
+
+    console.log("fecha actual")
+    console.log(fecha)
+
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+
+        to: token, 
+        //collapse_key: arraytokens,
+        
+        notification: {
+            title: fecha+' ### '+campos.titulo, 
+            body: campos.mensaje 
+        },
+        
+        data: {  //you can send only notification or only data(or include both)
+            my_key: 'my value',
+            my_another_key: 'my another value'
+        }/*,
+        token: arraytokens[arraytokens.length-1]*/
+    };
+
+    fcm.send(message, function(err, response){
+        if (err) {
+            console.log("Something has gone wrong!");
+        } else {
+            console.log("Successfully sent with response: ", response);
+        }
+    });
+
+};
